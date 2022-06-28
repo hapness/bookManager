2a58786bdaaa03b2cf9bc741b3d09c8e4172f187
hapness <87107173+hapness@users.noreply.github.com>
2022-06-28 13:39:13 +0800

    Delete footer.php

--- footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="container-fluid footer" >
-		<hr style="margin-top: 5rem;margin-bottom: 3rem; border: 1px solid #666;">
-		<p style="float: right;"><a href="#">Back to top</a></p>
-	    <p><a href="#">关于我们</a> &middot; <a href="#">联系我们</a> &middot; <a href="#">隐私声明</a></p>
-	</footer>
